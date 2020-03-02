c4a3801ef10d74b342d71b299a2660b3f022587a
ronitgavaskar <ronitgavaskar@gmail.com>
2020-03-01 19:28:41 -0800

    testing /

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 'use strict';
 
 // Imports dependencies and set up http server
-const
-  express = require('express'),
-  bodyParser = require('body-parser'),
-  app = express().use(bodyParser.json()); // creates express http server
+// const
+//   express = require('express'),
+//   bodyParser = require('body-parser'),
+//   app = express().use(bodyParser.json()); // creates express http server
+
+var app = express();
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 // Sets server port and logs message on success
 app.listen((process.env.PORT || 5000));
