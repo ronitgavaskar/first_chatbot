703898aeb09078958c8a942ed8253545a1581561
ronitgavaskar <ronitgavaskar@gmail.com>
2020-03-01 21:12:57 -0800

    testing

--- index.js
+++ index.js
@@ -111,7 +111,7 @@ function callSendAPI(sender_psid, response) {
     // make a POST request to the send API
     request({
         "uri": "https://graph.facebook.com/v2.6/me/messages",
-        "qs": { "access_token": PAGE_ACCESS_TOKEN },
+        "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
         "method": "POST",
         "json": req_body
     }, (err, res, body) => {
